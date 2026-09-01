e7ae6f48851e227f22d075a85a52f72a579b5bf3
Victoria Ayumi <victoriaayumikose@gmail.com>
2026-08-31 22:03:21 -0300

    aula-31/08

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
-console.log("Hello, World!");
-console.log("TypeScript version: 6.0.3");
+type User = {
+    name: string;
+    age:  number;
+};
+
+function makeOlder(user: User) {
+    return {...user, age: user.age + 1};
+}
+
+function printUser(user: User) {
+    console.log(`Name: ${user.name}, Age: ${user.age}`);
+}
+
+const user: User = { name: "Alice", age: 30 };
+
+console.log(makeOlder(user)); // Output: { name: 'Alice', age: 31 }
+printUser(user); // Output: Name: Alice, Age: 30    ]
